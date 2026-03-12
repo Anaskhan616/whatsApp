d243c3cd3e4790991a3b749e69adb97ce3646ceb
Anas Khan <officialanaskhan75500@gmail.com>
2026-03-12 23:07:31 +0530

    update project

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5002/api"
+      : "https://real-time-chat-app-production-65f6.up.railway.app/api",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5002"
-    : "/";
+    : "https://real-time-chat-app-production-65f6.up.railway.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
